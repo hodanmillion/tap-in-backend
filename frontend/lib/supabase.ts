259328a41674c34d90a2d707887cf08a06520881
hodanmillion <hodanmo15@gmail.com>
2026-01-29 05:26:03 -0500

    vfdgdgf

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { Platform } from 'react-native';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://bcijrphffxfolptawwpv.supabase.co';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjaWpycGhmZnhmb2xwdGF3d3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2NTEyNjMsImV4cCI6MjA4MzIyNzI2M30.M2yvQgcKsp34OWFojzM2OMmymAorFIVk4PNvauMWgSk';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://pcthejcpujqtnurpdmxs.supabase.co';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjdGhlamNwdWpxdG51cnBkbXhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MDE2NDYsImV4cCI6MjA4MTk3NzY0Nn0.t-M6nEh5fL9dfy3pDbLJ0eyPK9Ad1O4FdLNbVVsMNRc';
 
 const isServer = typeof window === 'undefined';
 
